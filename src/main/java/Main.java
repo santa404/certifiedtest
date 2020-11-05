9f9bc05ab7e629806f746bd6e1dd6e284cf04a20
daniel <Daniel.Dichte@t-online.de>
2020-11-05 14:25:13 +0100

    2.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -6,16 +7,14 @@ import org.openqa.selenium.support.ui.Select;
 
 public class Main {
     static WebDriver driver;
+    static WebDriver dropdown;
 
     public static void main(String[] args){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\marti\\Desktop\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","D:\\Program\\Chrom_Selenium_Driver\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://ct.informatik.haw-hamburg.de/tessa_tac_toe/");
-        clickCell(1);
-        clickSelect("player1-icon", "tessa_b");
-        clickCell(9);
-        clickCell(7);
-        //driver.findElement(By.id("cell-0")).click();
+        testy_MC_test_test_test();
+
         //driver.get("https://tessa.haw-hamburg.de");
         //driver.close();
 
@@ -28,8 +27,30 @@ public class Main {
     }
 
     public static void clickSelect(String select, String value) {
-        WebElement selectPlayer = driver.findElement(By.id(select));
-        selectPlayer.sendKeys(value);
+        driver.findElement(By.id(select)).click();
+        WebElement selectElement = driver.findElement(By.id(select));
+        selectElement = selectElement.findElement(By.className("gwt-ListBox"));
+        Select selectObject = new Select(selectElement);
+        selectObject.selectByValue(value);
     }
 
+    public static void testy_MC_test_test_test(){
+        try {
+            Thread.sleep(5000);
+            clickSelect("player1-icon", "tessa_b");
+            clickCell(0);
+            clickCell(7);
+            clickCell(5);
+            clickCell(14);
+            clickCell(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String whoWon = driver.findElement(By.id("playerwon")).getText();
+        Assert.assertTrue(whoWon.contains("player1 won the match!"));
+
+        String firstWin = driver.findElement(By.id("score-1")).getText();
+        Assert.assertTrue(firstWin.contains("1"));
+
+    }
 }
